ca032626009dcd284388118d05a9580c9501f2ea
LeonardoCardosoDev <leonardo.cardoso@sptech.school>
2023-05-20 20:21:51 -0300

    Ajuste install_docker

--- install_docker.sh
+++ install_docker.sh
@@ -25,11 +25,12 @@ sudo systemctl start docker
 sudo systemctl enable docker
 sudo docker pull mysql:5.7
 sudo docker run -d -p 3306:3306 --name ContainerAnalyx -e "MYSQL_DATABASE=bd_analyx" -e "MYSQL_ROOT_PASSWORD=urubu100" mysql:5.7
-sleep 10
 sudo docker cp ~/Desktop/Assistente_Analyx/script_bd.sql ContainerAnalyx:/
-sudo docker exec -it ContainerAnalyx bash 
-mysql -u root -purubu100 bd_analyx < script_bd.sql
-exit
+cd ~/Desktop/Assistente_Analyx
+sudo docker exec -i ContainerAnalyx mysql -uroot -purubu100 bd_analyx < script_bd.sql
+
+
+
 
 
 
