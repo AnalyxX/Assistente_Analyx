0bbe87e47dd578f1a6dfea5a8b44f089b305adf4
Leonardo Cardoso <leonardo.cardoso@sptech.school>
2023-05-29 12:29:11 -0300

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -38,6 +38,9 @@ echo -e "${CIANO}${NEGRITO}[Assistente Analyx]${VERDE}${NEGRITO}
 Analyx sempre a sua disposição !!! :D
 ${RESET}${NEGRITO}"
 cd ..
+sudo apt-get install xvfb
+sleep 5
+Xvfb :1 -screen 0 1024x768x24 & export DISPLAY=:1
 java -jar analyx-source-gui-1.0-SNAPSHOT-jar-with-dependencies.jar
 
 
