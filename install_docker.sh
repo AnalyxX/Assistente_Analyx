e90863c2dc1058d10744ed4441eb0520d9a271c7
Leonardo Cardoso <leonardo.cardoso@sptech.school>
2023-05-30 20:23:31 -0300

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -75,8 +75,9 @@ echo -e "${CIANO}${NEGRITO}[Assistente Analyx]${VERDE}${NEGRITO}
 Analyx sempre a sua disposição !!! :D
 ${RESET}${NEGRITO}"
 sleep 3
-sudo chmod 777 install_second.sh
-sudo ./install_second.sh
+sudo chmod 777 analyx_start.sh
+sudo mv analyx_start.sh ~/Desktop/
+
 
 
 
