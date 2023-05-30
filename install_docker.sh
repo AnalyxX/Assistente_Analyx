5599c79aca74bfca9fafe7183f004a15fb68d1fb
Leonardo Cardoso <leonardo.cardoso@sptech.school>
2023-05-30 18:55:59 -0300

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -75,10 +75,9 @@ ${RESET}${NEGRITO}"
 echo -e "${CIANO}${NEGRITO}[Assistente Analyx]${VERDE}${NEGRITO}
 Analyx sempre a sua disposição !!! :D
 ${RESET}${NEGRITO}"
-cd ~/Desktop/
-xhost +
+sudo xhost +
 sleep 10
-java -jar analyx-source-gui-1.0-SNAPSHOT-jar-with-dependencies.jar
+java -jar /home/ubuntu/Desktop/analyx-source-gui-1.0-SNAPSHOT-jar-with-dependencies.jar
 
 
 
